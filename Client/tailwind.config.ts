3b0eb6f837c481e13cb3aec7290d969dc5424296
MTalha250 <talhabinay@gmail.com>
2024-11-18 03:58:57 +0500

    update

--- Client/tailwind.config.ts
+++ Client/tailwind.config.ts
@@ -1,24 +1,33 @@
 import type { Config } from "tailwindcss";
 
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      fontFamily: {
+        slab: ["Montagu Slab", "sans-serif"],
+        sans: ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        primary: "#11224D",
+        primary2: "#193A6F",
+        primary3: "#2C599D",
+        primary4: "#5B84C4",
+        secondary: "#F98125",
+        secondary2: "#FB9B50",
+        background: "#A4BADD",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
